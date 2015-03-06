4d92e0a39b5909d10405a7103ec61178b7549ea5
JScott <jvscott@gmail.com>
2015-03-06 15:17:45 -0800

    Some basic crawling with simplecrawler

---
+++ lambda.js
@@ -0,0 +1,26 @@
+var Crawler = require("simplecrawler");
+
+exports.handler = function(event, context) {
+  var craas = new Crawler('www.reddit.com');
+  craas.initialPath = '/';
+  craas.initialPort = 80;
+  craas.initialProtocol = 'http';
+  craas.interval = 500; // default 250 milliseconds
+  craas.maxConcurrency = 3; // default 5
+  craas.maxDepth = 2; // default 0
+  craas.scanSubdomains = false; // default false
+  craas.downloadUnsupported = false; // default true
+
+  craas.on("fetchcomplete",function(queueItem, responseBuffer, response) {
+    console.log("I just received %s (%d bytes)",queueItem.url,responseBuffer.length);
+    console.log("It was a resource of type '%s'", response.headers['content-type']);
+
+    var mime = response.headers['content-type'].split(';')[0];
+    console.log(mime);
+    if (mime == 'text/html') {
+      console.log(responseBuffer.toString('utf-8'));
+    }
+  });
+
+  craas.start();
+};
